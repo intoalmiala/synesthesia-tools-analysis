406e7325dca4df1cb59e32ae80cb3877705ea9cc
Into Almiala <into.almiala@gmail.com>
2021-01-04 03:34:02 +0200

    update chi-square

--- chi-square.py
+++ chi-square.py
@@ -8,6 +8,7 @@ import numpy as np
 # Second argument is expected values of whole set
 INPUT_FILE_1 = sys.argv[1]
 INPUT_FILE_2 = sys.argv[2]
+RANDOM_SUBSET_AMOUNT = int(sys.argv[3])
 
 
 # Whole set
@@ -20,32 +21,46 @@ with open(INPUT_FILE_2) as f:
     B = json.load(f).values()
     B_size = sum(len(x) for x in B)
 
-
-# Define number of voxels and initialize distributions in each voxel
+# Divide the colorspace to n**3 cubes
 n = 2
+# Probability distribution for given subset
+probabilities = np.zeros(n**3)
+
+# Calculate chi-square for given subset
 O = np.zeros(n**3)
 E = np.zeros(n**3)
 
-# Count distributions in voxels
 for colors in B:
     for color in colors:
         x = int(round(color[0]/255))
         y = int(round(color[1]/255))
         z = int(round(color[2]/255))
-        O[n**2*x + n*y + z] += 1/len(colors)
+        O[n**2*x + n*y + z] += 1
+        probabilities[n**2*x + n*y + z] += 1
+probabilities /= B_size
 
 for colors in A:
     for color in colors:
         x = int(round(color[0]/255))
         y = int(round(color[1]/255))
         z = int(round(color[2]/255))
-        E[n**2*x + n*y + z] += 1/len(colors) / A_size * B_size
+        E[n**2*x + n*y + z] += 1 / A_size * B_size
 
-
-# Calculate chi-square
 chi2 = sum([(o - e)**2 / e for o, e in zip(O, E)])
 
-print(chi2)
+
+# Calculate chi-squares for given subset and random subsets
+random_chi2s = np.zeros(RANDOM_SUBSET_AMOUNT)
+
+from collections import Counter
+for i in range(RANDOM_SUBSET_AMOUNT):
+    O = np.array(sorted(list(Counter(np.random.choice(n**3, B_size, p=probabilities)).items())))[:,1]
+    random_chi2s[i] = sum([(o - e)**2 / e for o, e in zip(O, E)])
+
+
+print("chi2 of given subset:", chi2)
+print("mean chi2 of random subsets", random_chi2s.mean())
+print("difference:", abs(chi2 - random_chi2s.mean()))
 
 
 # Plot colors
